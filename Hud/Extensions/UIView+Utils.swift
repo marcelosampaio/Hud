bde00dbe7d7b1aa7697bd978f96e80e6469ec8e7
Marcelo Sampaio <marcelo@artlogica.com,br>
2018-12-27 15:45:23 -0200

    hud is created 🍄

---
+++ Hud/Extensions/UIView+Utils.swift
@@ -0,0 +1,41 @@
+//
+//  UIView+Utils.swift
+//  Hud
+//
+//  Created by Marcelo Sampaio on 27/12/18.
+//  Copyright © 2018 Marcelo Sampaio. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func showActivityLoading() {
+        let shadowView = UIView(frame:CGRect(x: 0, y:0, width: 90, height:90))
+        shadowView.center = self.center
+        shadowView.layer.cornerRadius = 45
+        shadowView.layer.masksToBounds = true
+        
+        shadowView.tag = -998;
+        shadowView.backgroundColor = UIColor.black
+
+        
+        let activity = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.whiteLarge)
+        activity.center = self.center
+        activity.tag = -999
+        
+        UIApplication.shared.keyWindow?.rootViewController?.view.addSubview(shadowView)
+        
+        self.addSubview(shadowView)
+        self.addSubview(activity)
+        activity.startAnimating()
+    }
+    
+    func hideActivityLoading() {
+        self.alpha = 1.0
+        self.viewWithTag(-998)?.removeFromSuperview()
+        self.viewWithTag(-999)?.removeFromSuperview()
+    }
+    
+}
+
--- Hud/ViewController.swift
+++ Hud/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var status = false
+    
+    @IBOutlet weak var goButton: UIBarButtonItem!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    
+    @IBAction func goAction(_ sender: Any) {
+        if !status {
+            self.view.showActivityLoading()
+            status = true
+            goButton.title = "Stop"
+        }else{
+            self.view.hideActivityLoading()
+            status = false
+            goButton.title = "Go"
+        }
+    }
+    
+    
+    
+    
 }
 
