96d45a91e5722d66c79261586ff59ea9c19b5898
Marcelo Sampaio <marcelo@artlogica.com,br>
2018-12-27 15:23:23 -0200

    Initial Commit

---
+++ Hud/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hud
+//
+//  Created by Marcelo Sampaio on 27/12/18.
+//  Copyright © 2018 Marcelo Sampaio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
